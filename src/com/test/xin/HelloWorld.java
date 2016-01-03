1d371055a895f6973d40e1c800aca9eaec51f8d4
ahdongxin <ahdongxin@qq.com>
2016-01-03 12:56:38 +0800

    1st class
    test for 1st class

---
+++ src/com/test/xin/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.test.xin;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+	}
+
+}
